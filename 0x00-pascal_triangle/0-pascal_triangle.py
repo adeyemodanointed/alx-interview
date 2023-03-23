3f435f55784b1cb2d96622fabb510dfcc1b0c8a5
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-03-23 22:02:32 +0000

    task complete

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from math import factorial
+"""Function to generate pascal triangle"""
+
+def pascal_triangle(n):
+    our_list = []
+    for i in range(n):
+        mini_list = []
+        for j in range(i + 1):
+            mini_list.append(factorial(i)//(factorial(j)*factorial(i-j)))
+        our_list.append(mini_list)
+    return our_list
