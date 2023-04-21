555d66c49d456393cff423c4db38071b8c4732f2
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-21 00:45:17 +0000

    check numbers with bitwise operations

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,8 +4,19 @@
 
 def validUTF8(data):
     """Check for data with bit higher than 11111111 in decimal"""
+    num_bytes = 0
     for i in data:
-        if (i > 255):
-            return False
-    else:
-        return True
+        if num_bytes == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                num_bytes = 1
+            elif i >> 4 == 0b1110:
+                num_bytes = 2
+            elif i >> 3 == 0b11110:
+                num_bytes = 3
+            elif i >> 7:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+        return num_bytes == 0
