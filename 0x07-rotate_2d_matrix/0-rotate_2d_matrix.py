c86cddef98bc37ec29ec5bcdd4183040df9be507
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-12 05:47:36 +0000

    pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,15 +4,15 @@
 
 def rotate_2d_matrix(matrix):
     """Brute-Force method"""
-    l, r = 0, len(matrix) - 1
+    left, r = 0, len(matrix) - 1
 
-    while l < r:
-        for i in range (r - l):
-            t, b = l, r
-            t_l = matrix[t][l + i]
-            matrix[t][l + i] = matrix[b - i][l]
-            matrix[b - i][l] = matrix[b][r - i]
+    while left < r:
+        for i in range(r - left):
+            t, b = left, r
+            t_l = matrix[t][left + i]
+            matrix[t][left + i] = matrix[b - i][left]
+            matrix[b - i][left] = matrix[b][r - i]
             matrix[b][r - i] = matrix[t + i][r]
             matrix[t + i][r] = t_l
         r -= 1
-        l += 1
+        left += 1
