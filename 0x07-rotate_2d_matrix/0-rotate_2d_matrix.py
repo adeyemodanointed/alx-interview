9ca7954a105a60229087002caf26ca744e15f726
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-05-12 05:45:20 +0000

    change

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """Rotate 2D-matrix algorithm"""
-import copy
 
 
 def rotate_2d_matrix(matrix):
     """Brute-Force method"""
-    rotated_matrix = copy.deepcopy(matrix)
-    mat_len = len(matrix)
-    for i in range(mat_len):
-        for j in range(mat_len):
-            matrix[i][j] = rotated_matrix[mat_len - 1 - j][i]
+    l, r = 0, len(matrix) - 1
+
+    while l < r:
+        for i in range (r - l):
+            t, b = l, r
+            t_l = matrix[t][l + i]
+            matrix[t][l + i] = matrix[b - i][l]
+            matrix[b - i][l] = matrix[b][r - i]
+            matrix[b][r - i] = matrix[t + i][r]
+            matrix[t + i][r] = t_l
+        r -= 1
+        l += 1
